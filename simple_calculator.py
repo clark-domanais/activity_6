8e3ee9a7bc22ace98dd4f199b997d40714c3f0cd
Clark Domanais <clarkdomanais@gmail.com>
2026-05-05 02:07:17 +0800

    added multiplication and division

--- simple_calculator.py
+++ simple_calculator.py
@@ -6,3 +6,7 @@ class Calculator:
 
     def subtract(self, first_number, second_number):
         return first_number - second_number
+    def multiply(self, first_number, second_number):
+        return first_number * second_number
+    def divide(self, first_number, second_number):
+        return first_number / second_number
