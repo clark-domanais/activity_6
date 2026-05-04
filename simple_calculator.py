fd7035ddd1ea9a1e68c7d1d190be9626aec40177
Clark Domanais <clarkdomanais@gmail.com>
2026-05-05 03:35:46 +0800

    changed the name of the class

--- simple_calculator.py
+++ simple_calculator.py
@@ -12,7 +12,7 @@ class Calculator:
         return first_number / second_number
     def power(self, first_number, second_number):
         return first_number ** second_number
-    def modulos(self, first_number, second_number):
+    def modulo(self, first_number, second_number):
         return first_number % second_number
     def floor_division(self, first_number, second_number):
         return first_number // second_number
