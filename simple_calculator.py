b30b38c6ff459621220627ff0e5d7e5b8515afc2
Clark Domanais <clarkdomanais@gmail.com>
2026-05-05 03:02:33 +0800

    changed the function of square root

--- simple_calculator.py
+++ simple_calculator.py
@@ -14,3 +14,9 @@ class Calculator:
         return first_number ** second_number
     def modulos(self, first_number, second_number):
         return first_number % second_number
+    def floor_division(self, first_number, second_number):
+        return first_number // second_number
+    def square_root(self, first_number):
+        if number >= 0:
+            return math.sqrt(first_number)
+        return first_number ** 0.5
