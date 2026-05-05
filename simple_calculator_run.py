fd23b8929ae89ed9bfab55d8f209dbdb7ce374fd
Clark Domanais <clarkdomanais@gmail.com>
2026-05-06 00:13:24 +0800

    changed the condition in the if-else statement and added an else function to handle the imput error

--- simple_calculator_run.py
+++ simple_calculator_run.py
@@ -6,28 +6,31 @@ if __name__ == "__main__":
 
     while True:
         try:
-            operation = input("Enter operation (Addition, Subtraction, Multiplication, Division, Modulo, Floor Division, Square Root): ")
-            if operation == "Square Root":
+            operation = input("Enter operation (+, -, *, /, %, //, sqrt)): ")
+            if operation == "sqrt":
                 number = int(input("Enter number: "))
                 result = calculator.square_root(number)
             else:
                 first_number = int(input("Enter first number: "))
                 second_number = int(input("Enter second number: "))
 
-                if operation == "Addition":
+                if operation == "+":
                     result = calculator.add(first_number, second_number)
-                elif operation == "Subtraction":
+                elif operation == "-":
                     result = calculator.subtract(first_number, second_number)
-                elif operation == "Multiplication":
+                elif operation == "*":
                     result = calculator.multiply(first_number, second_number)
-                elif operation == "Division":
+                elif operation == "/":
                     result = calculator.divide(first_number, second_number)
-                elif operation == "Modulo":
+                elif operation == "%":
                     result = calculator.modulo(first_number, second_number)
-                elif operation == "Floor Division":
+                elif operation == "//":
                     result = calculator.floor_division(first_number, second_number)
+                else:
+                    print("Invalid operation")
 
             print(f"Result: {result}")
+
         except ValueError:
             print("Invalid input")
         if input("Continue? (Y/N): ").lower() != "y":
